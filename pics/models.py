cdc2b5157d6987e82ca2061aff148893950bf82d
Janice-M <janiceink001@gmail.com>
2019-09-01 21:38:37 +0300

    class Profile

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=350) 
+    profile_pic = models.ImageField(upload_to='ProfilePicture/')
+    profile_avatar = models.ImageField(upload_to='AvatorPicture/')
+    date = models.DateTimeField(auto_now_add=True, null= True)  
