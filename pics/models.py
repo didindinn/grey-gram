654c48077da8a0afa3bcf9d01ecc3018ee0b55f1
Janice-M <janiceink001@gmail.com>
2019-09-01 21:39:20 +0300

    class Profile

--- pics/models.py
+++ pics/models.py
@@ -14,3 +14,7 @@ class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='ProfilePicture/')
     profile_avatar = models.ImageField(upload_to='AvatorPicture/')
     date = models.DateTimeField(auto_now_add=True, null= True)  
+    
+    '''getting a specific profile results results'''
+    def __str__(self):
+        return self.profile.user
