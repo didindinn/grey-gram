e245fe2d0b47bd76826723fb6a21aa412b2e57fc
Janice-M <janiceink001@gmail.com>
2019-09-01 21:51:02 +0300

    creating the url paths in app

--- pics/urls.py
+++ pics/urls.py
@@ -6,4 +6,16 @@ from django.shortcuts import render, redirect
 
 #now let us create the juicy paths 
 
+urlpatterns=[
+    path('',views.index, name='index'),
+    path('explore',views.explore,name ='explore'),
+    path('notification',views.notification,name ='notification'),
+    path('profile',views.profile,name ='profile'),
+    path('login',views.login,name ='login'),
+    path('logout',views.index,{'next_page': 'accounts:login'}, name='logout')
+    path('upload',views.upload,name ='upload'),
+    
+]
 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
