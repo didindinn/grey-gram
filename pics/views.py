6980f87674d5227635ba72afe8fd21c933958961
Janice-M <janiceink001@gmail.com>
2019-09-01 21:52:59 +0300

    working on views.py

--- pics/views.py
+++ pics/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.conf.urls import url,include
+from django.contrib.auth import authenticate, login, logout
+from .forms import PostForm
+from django.conf.urls.static import static
+from .models import Profile, Image
+from django.contrib.auth.models import User
+from . import models
+from annoying.decorators import ajax_request
 
-# Create your views here.
+from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required
