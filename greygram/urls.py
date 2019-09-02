648962e7028aeb5d89e6f09a3b81fee3df9043d7
Janice-M <janiceink001@gmail.com>
2019-09-02 17:25:03 +0300

    dear lord

--- greygram/urls.py
+++ greygram/urls.py
@@ -1,32 +1,26 @@
-"""greygram URL Configuration
+"""insta URL Configuration"""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 
-from django.conf.urls import url, include
+from django.urls import include, path
 
-from pics import views
+from gram import views
 
 from django.conf.urls.static import static
 
+'''Here we have imported (include)-: this allows us to add the gram url pattern
+as done below this is to avoid filling up all our urls in this page'''
+
+''' End Of Import'''
+#-------------------------------#
+
+
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('',include('pics.urls')),
-    url(r'^accounts/',include ('registration.backends.simple.urls')),
-    
+
+    path('admin/', admin.site.urls),
+    path('', include('gram.urls')),
+
+    #url for registration
+    path('accounts/', include('registration.backends.simple.urls')),
 
 ]
-
-
